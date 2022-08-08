08ed03641b45a5db04f0512d57c81c7d7487721f
JohnGuilding <jwguilding@gmail.com>
2022-08-08 21:11:25 +0100

    Add loading spinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineLoading } from "react-icons/ai";
+
+const LoadingSpinner = () => (
+    <AiOutlineLoading size="30" className="animate-spin" />
+);
+
+export default LoadingSpinner;
