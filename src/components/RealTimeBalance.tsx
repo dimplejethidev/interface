14c6e40ab274bd7f9eb642ce0467dc3dcc6a5112
joshkokatnur <jkokatnur@gmail.com>
2022-10-30 01:30:18 -0400

    Added deposit confirmation checkbox to swap widget Swap widget also checks user balance is greater than minimum amount

---
+++ src/components/RealTimeBalance.tsx
@@ -0,0 +1,85 @@
+import { BigNumber, ethers } from "ethers"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { useAccount, useProvider } from "wagmi"
+import { TokenOption } from "../../types/TokenOption";
+
+const ANIMATION_MINIMUM_STEP_TIME = 100;
+const REFRESH_INTERVAL = 300; // 300 * 100 = 30000 ms = 30 s
+
+interface RealTimeBalanceProps {
+    token: TokenOption;
+    setBalance: Dispatch<SetStateAction<BigNumber>>;
+}
+
+const RealTimeBalance = ({ token, setBalance }: RealTimeBalanceProps) => {
+    const [flowRate, setFlowRate] = useState<BigNumber>(ethers.BigNumber.from(0));
+    const provider = useProvider();
+    const { address } = useAccount();
+
+    async function refresh() {
+        // refresh vars
+        const tokenABI = [
+            "function realtimeBalanceOf(address account, uint256 timestamp) public view returns (int256 availableBalance, uint256 deposit, uint256 owedDeposit)",
+        ];
+        if (address) {
+            const tokenContract = new ethers.Contract(
+                token.address,
+                tokenABI,
+                provider
+            );
+            const currentTimestampBigNumber = ethers.BigNumber.from(
+                new Date().valueOf() // Milliseconds elapsed since UTC epoch, disregards timezone.
+            );
+
+            // set token state
+            const initialBalance = (
+                await tokenContract.realtimeBalanceOf(
+                    address,
+                    currentTimestampBigNumber.div(1000).toString()
+                )
+            ).availableBalance;
+            const futureBalance = (
+                await tokenContract.realtimeBalanceOf(
+                    address,
+                    currentTimestampBigNumber
+                        .div(1000)
+                        .add(
+                            (REFRESH_INTERVAL * ANIMATION_MINIMUM_STEP_TIME) /
+                            1000
+                        )
+                        .toString()
+                )
+            ).availableBalance;
+            setBalance(initialBalance);
+            setFlowRate(
+                futureBalance.sub(initialBalance).div(REFRESH_INTERVAL)
+            );
+        }
+    }
+
+    // REFRESH(in milliseconds) = REFRESH_INTERVAL * ANIMATION_MINIMUM_STEP_TIME
+    const [time, setTime] = useState(REFRESH_INTERVAL);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTime(time + 1);
+            if (time >= REFRESH_INTERVAL) {
+                setTime(0);
+                refresh();
+            }
+
+            // animate frame
+            setBalance((c) => c.add(flowRate));
+        }, ANIMATION_MINIMUM_STEP_TIME);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [time]);
+
+    useEffect(() => {
+        refresh();
+    }, [address]);
+
+    return (<div/>)
+}
+
+export default RealTimeBalance;
--- src/components/widgets/CreateStreamWidget.tsx
+++ src/components/widgets/CreateStreamWidget.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { SetStateAction, useEffect, useRef, useState } from "react";
 import { BigNumber, ethers } from "ethers";
 import { Framework } from "@superfluid-finance/sdk-core";
 import { useAccount, useNetwork, useProvider, useSigner } from "wagmi";
@@ -13,6 +13,8 @@ import { useStore } from "../../store";
 import PricingField from "../PricingField";
 import flowrates from "../../utils/flowrates";
 import TransactionButton from "../TransactionButton";
+import { BsCheckLg } from 'react-icons/bs'
+import RealTimeBalance from "../RealTimeBalance";
 
 interface CreateStreamWidgetProps {
     showToast: (type: ToastType) => {};
@@ -39,6 +41,14 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
     const token0Flow = useRef(BigNumber.from(0));
     const token1Flow = useRef(BigNumber.from(0));
     const userToken0Flow = useRef(BigNumber.from(0));
+    const [minBalance, setMinBalance] = useState(BigNumber.from(0));
+    const [deposit, setDeposit] = useState(BigNumber.from(0));
+
+    // buffer confirmation
+    const [acceptedBuffer, setAcceptedBuffer] = useState(false);
+
+    // user vars
+    const [outboundTokenBalance, setOutboundTokenBalance] = useState(BigNumber.from(0));
 
     const swap = async () => {
         try {
@@ -73,7 +83,7 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
                     });
                     const result = await createFlowOperation.exec(signer);
                     await result.wait();
-    
+
                     console.log("Stream created: ", result);
                 } else {
                     // create stream
@@ -84,10 +94,10 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
                     });
                     const result = await createFlowOperation.exec(signer);
                     await result.wait();
-    
+
                     console.log("Stream created: ", result);
                 }
-                
+
                 showToast(ToastType.Success);
                 setLoading(false);
             }
@@ -122,6 +132,19 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
             setToken1Price(0);
             setPriceMultiple(BigNumber.from(0));
         }
+
+        // calculate deposit
+        if (swapFlowRate != "") {
+            // assume 1 hr length for deposit // TODO: mainnet is 4 hrs, detect network and adjust deposit period
+            const oneHourStream = BigNumber.from(swapFlowRate).mul(3600);
+            setDeposit(oneHourStream);
+
+            // had hard time determining min balance, default to 2 hours of streaming for now // TODO: detect network and adjust
+            setMinBalance(oneHourStream.mul(2));
+        }
+
+        // reset deposit agreement
+        //setAcceptedBuffer(false);
 
         await new Promise((res) => setTimeout(res, 900));
         setRefreshingPrice(false);
@@ -182,7 +205,7 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
         }
 
         refresh();
-    }, [store.inboundToken, store.outboundToken]);
+    }, [store.inboundToken, store.outboundToken, address]);
 
     // refresh spot pricing upon user input
     useEffect(() => {
@@ -194,9 +217,10 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
 
         refresh();
     }, [swapFlowRate]);
- 
+
     return (
         <section className="flex flex-col items-center w-full">
+            <RealTimeBalance token={store.outboundToken} setBalance={setOutboundTokenBalance} />
             <WidgetContainer title="Swap">
                 <TokenSelectField />
                 <NumberEntryField
@@ -215,14 +239,65 @@ const CreateStreamWidget = ({ showToast }: CreateStreamWidgetProps) => {
                     swapFlowRate={swapFlowRate}
                     poolExists={poolExists}
                 />
+                {
+                    poolExists && swapFlowRate &&
+                    <div>
+                        {
+                            minBalance.lt(outboundTokenBalance)
+                                ?
+                                <div
+                                    className={
+                                        "text-xs px-6 py-4 rounded-2xl space-y-4 transition-all duration-500 "
+                                        + (acceptedBuffer ? 'bg-gray-100 text-gray-900/90' : 'bg-red-100 text-red-900/90')
+                                    }
+                                >
+                                    <p>
+                                        {'If you do not cancel this stream before your balance reaches zero, you will lose your ' + ethers.utils.formatEther(deposit) + ' ' + store.outboundToken.label + ' buffer.'}
+                                    </p>
+                                    <div className="flex space-x-2 pb-1 items-center">
+                                        <button
+                                            className={
+                                                "flex items-center justify-center text-white border-[2px] rounded-md w-5 h-5 "
+                                                + (acceptedBuffer ? 'border-aqueductBlue bg-aqueductBlue' : 'border-red-900/90')
+                                            }
+                                            onClick={() => { setAcceptedBuffer(!acceptedBuffer); }}
+                                        >
+                                            {acceptedBuffer && <BsCheckLg size={8} />}
+                                        </button>
+                                        <p className="text-xs">
+                                            Yes, I understand the risk
+                                        </p>
+                                    </div>
+                                </div>
+                                :
+                                <div
+                                    className={
+                                        "text-xs px-6 py-4 rounded-2xl space-y-4 transition-all duration-500 bg-red-100 text-red-900/90 "
+                                    }
+                                >
+                                    {
+                                        deposit.gt(outboundTokenBalance)
+                                            ?
+                                            <p>
+                                                {'You do not have enough balance to cover the ' + ethers.utils.formatEther(deposit) + ' ' + store.outboundToken.label + ' buffer.'}
+                                            </p>
+                                            :
+                                            <p>
+                                                {'You need to leave enough balance to stream for 2 hours.'}
+                                            </p>
+                                    }
+                                </div>
+                        }
+                    </div>
+                }
                 <TransactionButton
                     title={userToken0Flow.current.gt(0) ? 'Update Swap' : 'Swap'}
                     loading={loading}
                     onClickFunction={swap}
-                    errorMessage={!poolExists ? 'Select valid token pair' : (!swapFlowRate || BigNumber.from(swapFlowRate).lte(0) ? 'Enter flow rate' : undefined)}
+                    errorMessage={!poolExists ? 'Select valid token pair' : (!swapFlowRate || BigNumber.from(swapFlowRate).lte(0) ? 'Enter flow rate' : (!acceptedBuffer ? (userToken0Flow.current.gt(0) ? 'Update Swap' : 'Swap') : undefined))}
                 />
             </WidgetContainer>
-        </section>
+        </section >
     );
 };
 
