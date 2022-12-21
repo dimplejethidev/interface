ccb99d8f21fd3e42d3d84c51b9841dc34a2650d9
JohnGuilding <jwguilding@gmail.com>
2022-12-21 17:01:27 +0000

    Add key for props & format welcome component

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import type { NextPage } from "next";
 import { useState } from "react";
 import { IoCheckmark, IoChevronForwardOutline } from "react-icons/io5";
@@ -20,36 +21,36 @@ interface SlideItem {
 const ICON_SIZE = 150;
 const slideItems: SlideItem[] = [
     {
-        title: 'Welcome to aqueduct!',
-        text: 'Aqueduct is the trading protocol for real-time finance - swap tokens continuously, on a per second basis.',
-        icon: <TiWaves size={ICON_SIZE + 35} />
+        title: "Welcome to aqueduct!",
+        text: "Aqueduct is the trading protocol for real-time finance - swap tokens continuously, on a per second basis.",
+        icon: <TiWaves size={ICON_SIZE + 35} />,
     },
     {
-        title: '1. Connect your wallet',
-        text: 'Click the button in the top-left corner to connect your wallet.',
-        icon: <IoIosWallet size={ICON_SIZE} />
+        title: "1. Connect your wallet",
+        text: "Click the button in the top-left corner to connect your wallet.",
+        icon: <IoIosWallet size={ICON_SIZE} />,
     },
     {
-        title: '2. Request some tokens',
-        text: 'You\'ll need some super tokens. Conveniently, we\'ll "pay" you via a continuous Superfluid stream.',
-        icon: <GiTwoCoins size={ICON_SIZE} />
+        title: "2. Request some tokens",
+        text: "You'll need some super tokens. Conveniently, we'll \"pay\" you via a continuous Superfluid stream.",
+        icon: <GiTwoCoins size={ICON_SIZE} />,
     },
     {
-        title: '3. Start a swap',
-        text: 'Simply start a swap by selecting a token and specifying a flow rate. Every second, you\'ll be sending some tokens and receiving the swapped tokens.',
-        icon: <TbArrowsRightLeft size={ICON_SIZE - 15} />
+        title: "3. Start a swap",
+        text: "Simply start a swap by selecting a token and specifying a flow rate. Every second, you'll be sending some tokens and receiving the swapped tokens.",
+        icon: <TbArrowsRightLeft size={ICON_SIZE - 15} />,
     },
     {
-        title: '4. Watch your balances',
+        title: "4. Watch your balances",
         text: 'The "My Streams" tab will show your positions. You can click a position to show the amounts of each token that have been streamed.',
-        icon: <VscGraphLine size={ICON_SIZE - 15} />
+        icon: <VscGraphLine size={ICON_SIZE - 15} />,
     },
     {
-        title: 'Need more help?',
+        title: "Need more help?",
         text: 'Click the button in the bottom-right corner to see your progress in this tutorial. Click one of the "?" icons to get help for that step.',
-        icon: <AiOutlineQuestionCircle size={ICON_SIZE} />
-    }
-]
+        icon: <AiOutlineQuestionCircle size={ICON_SIZE} />,
+    },
+];
 
 const Slide = ({ slideItem }: { slideItem: SlideItem }) => (
     <div className="space-y-2">
@@ -59,11 +60,9 @@ const Slide = ({ slideItem }: { slideItem: SlideItem }) => (
         <p className="text-black dark:text-white text-2xl font-semibold leading-relaxed">
             {slideItem.title}
         </p>
-        <p className="leading-relaxed">
-            {slideItem.text}
-        </p>
+        <p className="leading-relaxed">{slideItem.text}</p>
     </div>
-)
+);
 
 const Welcome: NextPage = () => {
     const [slideIndex, setSlideIndex] = useState(0);
@@ -74,24 +73,34 @@ const Welcome: NextPage = () => {
             <WidgetContainer isUnbounded={false}>
                 <div className="space-y-6 px-3 md:px-0">
                     <div className="overflow-hidden">
-                        {
-                            slideItems.map((e, i) => (
-                                // eslint-disable-next-line no-nested-ternary
-                                <div className={`transition-all ${i < slideIndex ? 'duration-300 -translate-x-4 opacity-0 h-0 ' : (i > slideIndex ? ' translate-x-8 opacity-0 h-0 overflow-hidden ' : ' duration-1000 ')}`}>
-                                    <Slide slideItem={e} />
-                                </div>
-                            ))
-                        }
+                        {slideItems.map((e, i) => (
+                            <div
+                                className={`transition-all ${
+                                    // eslint-disable-next-line no-nested-ternary
+                                    i < slideIndex
+                                        ? "duration-300 -translate-x-4 opacity-0 h-0 "
+                                        : i > slideIndex
+                                        ? " translate-x-8 opacity-0 h-0 overflow-hidden "
+                                        : " duration-1000 "
+                                }`}
+                                key={i}
+                            >
+                                <Slide slideItem={e} />
+                            </div>
+                        ))}
                     </div>
 
                     <div className="flex items-end space-x-2">
-                        {
-                            slideItems.map((e, i) => (
-                                <div className={
-                                    `transition-all duration-1000 w-6 md:w-12 h-2 rounded-full ${i <= slideIndex ? " bg-aqueductBlue/70 " : " bg-aqueductBlue/20 "}`
-                                } />
-                            ))
-                        }
+                        {slideItems.map((e, i) => (
+                            <div
+                                className={`transition-all duration-1000 w-6 md:w-12 h-2 rounded-full ${
+                                    i <= slideIndex
+                                        ? " bg-aqueductBlue/70 "
+                                        : " bg-aqueductBlue/20 "
+                                }`}
+                                key={i}
+                            />
+                        ))}
                         <div className="flex grow" />
                         <button
                             type="button"
@@ -99,19 +108,17 @@ const Welcome: NextPage = () => {
                             onClick={() => {
                                 if (slideIndex >= slideItems.length - 1) {
                                     // reroute to swap page when done
-                                    router.push('/');
+                                    router.push("/");
                                 } else {
                                     setSlideIndex((i) => i + 1);
                                 }
                             }}
                         >
-                            {
-                                slideIndex >= slideItems.length - 1
-                                    ?
-                                    <IoCheckmark size={24} />
-                                    :
-                                    <IoChevronForwardOutline size={24} />
-                            }
+                            {slideIndex >= slideItems.length - 1 ? (
+                                <IoCheckmark size={24} />
+                            ) : (
+                                <IoChevronForwardOutline size={24} />
+                            )}
                         </button>
                     </div>
                 </div>
