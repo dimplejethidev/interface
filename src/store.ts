a9c97af9a784a5c360e2c28beb2f24168c17eedd
JohnGuilding <jwguilding@gmail.com>
2023-01-11 10:25:00 +0100

    Set default flowrate to per week

--- src/store.ts
+++ src/store.ts
@@ -6,33 +6,33 @@ import flowrates from "./utils/flowrates";
 import tokens from "./utils/tokens";
 
 interface StoreState {
-    outboundToken: TokenOption;
-    inboundToken: TokenOption;
-    upgradeDowngradeToken: TokenOption;
-    selectedToken: Token;
-    flowrateUnit: GenericDropdownOption;
-    setOutboundToken: (token: TokenOption) => void;
-    setInboundToken: (token: TokenOption) => void;
-    setUpgradeDowngradeToken: (token: TokenOption) => void;
-    setSelectedToken: (token: Token) => void;
-    setFlowrateUnit: (flowrateUnit: GenericDropdownOption) => void;
+  outboundToken: TokenOption;
+  inboundToken: TokenOption;
+  upgradeDowngradeToken: TokenOption;
+  selectedToken: Token;
+  flowrateUnit: GenericDropdownOption;
+  setOutboundToken: (token: TokenOption) => void;
+  setInboundToken: (token: TokenOption) => void;
+  setUpgradeDowngradeToken: (token: TokenOption) => void;
+  setSelectedToken: (token: Token) => void;
+  setFlowrateUnit: (flowrateUnit: GenericDropdownOption) => void;
 }
 
 // eslint-disable-next-line import/prefer-default-export
 export const useStore = create<StoreState>()((set) => ({
-    outboundToken: tokens.find((t) => t.value === Token.fDAIxp)!,
-    inboundToken: tokens.find((t) => t.value === Token.fUSDCxp)!,
-    upgradeDowngradeToken: tokens.find((t) => t.value === Token.fDAIxp)!,
-    selectedToken: Token.fDAIxp,
-    flowrateUnit: flowrates[0],
-    setOutboundToken: (outboundToken: TokenOption) =>
-        set((state) => ({ ...state, outboundToken })),
-    setInboundToken: (inboundToken: TokenOption) =>
-        set((state) => ({ ...state, inboundToken })),
-    setUpgradeDowngradeToken: (upgradeDowngradeToken: TokenOption) =>
-        set((state) => ({ ...state, upgradeDowngradeToken })),
-    setSelectedToken: (selectedToken: Token) =>
-        set((state) => ({ ...state, selectedToken })),
-    setFlowrateUnit: (flowrateUnit: GenericDropdownOption) =>
-        set((state) => ({ ...state, flowrateUnit })),
+  outboundToken: tokens.find((t) => t.value === Token.fDAIxp)!,
+  inboundToken: tokens.find((t) => t.value === Token.fUSDCxp)!,
+  upgradeDowngradeToken: tokens.find((t) => t.value === Token.fDAIxp)!,
+  selectedToken: Token.fDAIxp,
+  flowrateUnit: flowrates[4],
+  setOutboundToken: (outboundToken: TokenOption) =>
+    set((state) => ({ ...state, outboundToken })),
+  setInboundToken: (inboundToken: TokenOption) =>
+    set((state) => ({ ...state, inboundToken })),
+  setUpgradeDowngradeToken: (upgradeDowngradeToken: TokenOption) =>
+    set((state) => ({ ...state, upgradeDowngradeToken })),
+  setSelectedToken: (selectedToken: Token) =>
+    set((state) => ({ ...state, selectedToken })),
+  setFlowrateUnit: (flowrateUnit: GenericDropdownOption) =>
+    set((state) => ({ ...state, flowrateUnit })),
 }));
